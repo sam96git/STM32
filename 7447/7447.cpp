6a6a810313b616ef51109391ded26bb1a8e9d2af
sam96git <sashyamanand099@gmail.com>
2025-01-06 12:51:23 +0530

    Create 7447.cpp

---
+++ 7447/7447.cpp
@@ -0,0 +1,24 @@
+#Code by GVV Sharma
+#Author-Shyam Anand
+#December 24, 2024
+#released under GNU GPL
+
+#include<Arduino.h>
+void disp_7447(int D, int C, int B, int A)
+{
+  digitalWrite(PB3, A); 
+  digitalWrite(PB4, B); 
+  digitalWrite(PB5, C); 
+  digitalWrite(PB6, D); 
+
+}
+void setup() {
+    pinMode(PB3, OUTPUT);  
+    pinMode(PB4, OUTPUT);
+    pinMode(PB5, OUTPUT);
+    pinMode(PB6, OUTPUT);
+}
+
+void loop() {
+        disp_7447(0,0,0,0);
+}
